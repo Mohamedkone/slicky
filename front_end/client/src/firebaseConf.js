e6e2bf1e2de2109b2d8fdcfa65c765b27ee806d7
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-02-07 23:01:17 -0500

    sign in fuction set up

--- front_end/client/src/firebaseConf.js
+++ front_end/client/src/firebaseConf.js
@@ -4,11 +4,18 @@ import firebase from 'firebase/app';
   
 function SignIn(props) {
   
+    const signInWithGoogle = () => {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      props.auth.signInWithPopup(provider);
+    }
+  
     return (
-     
+      <>
+        <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      
+      </>
     )
   
   }
-  
-  
+
   export default SignIn;
