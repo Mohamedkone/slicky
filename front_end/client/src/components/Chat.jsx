f640621177a0d0d62cf303d5d225d528a19ffdb1
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-02-07 23:05:25 -0500

    boiler for chat component

---
+++ front_end/client/src/components/Chat.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import firebase from "firebase";
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+function Chat(props) {
+      
+return  
+  }
+  
+  export default Chat;
+  
