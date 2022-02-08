b27fab4fdffbc5277845b61823098b050a8f44ec
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-02-07 23:06:46 -0500

    Set function to push message

--- front_end/client/src/components/Chat.jsx
+++ front_end/client/src/components/Chat.jsx
@@ -4,7 +4,26 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 function Chat(props) {
       
-return  
+
+    const messagesRef = props.firestore.collection('messages');
+    const query = messagesRef.orderBy('createdAt').limit(25);
+  
+    const [messages] = useCollectionData(query, { idField: 'id' });
+    const [formValue, setFormValue] = useState("")
+  
+    const sendMessage = async(e) =>{
+      e.preventDefault()
+      console.log(e)
+      console.log(props.auth)
+      await messagesRef.add({
+        text: formValue,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+    }
+    return 0
+  
+
+  
   }
   
   export default Chat;
