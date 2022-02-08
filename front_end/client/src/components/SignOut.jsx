9a61e2ac07e2b1bbbef45c4a3db6de340dd5fe54
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-02-07 23:04:06 -0500

    finished function for sign out

--- front_end/client/src/components/SignOut.jsx
+++ front_end/client/src/components/SignOut.jsx
@@ -4,6 +4,9 @@ import firebase from 'firebase/app';
 import React from 'react';
 
 function SignOut(props) {
-
+    return props.auth.currentUser && (
+      <button className="sign-out" onClick={() => props.auth.signOut()}>Sign Out</button>
+    )
+  }
 
 export default SignOut;
