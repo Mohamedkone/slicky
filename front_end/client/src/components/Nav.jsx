e4561987e3b5162b0e7a9faf05e9c9183d449fe0
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-02-09 15:05:31 -0500

    completed reg

--- front_end/client/src/components/Nav.jsx
+++ front_end/client/src/components/Nav.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Burger from '../assets/menu.svg'
 import SignOut from './SignOut';
 function Nav(props) {
+    let decision = props.decision
   return (
       <nav>
-          <div className="burger"><img src={Burger} alt="" /></div>
+          {decision?<div className="burger"><img src={Burger} alt="" /></div>:<div></div>}
           <h2>{props.title}</h2>
-          <div className="option1"><img src={props.option} alt="" /></div>
+          {decision?<div className="option1"><img src={props.option} alt="" /></div>:<div></div>}
       </nav>
   )
 }
