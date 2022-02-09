131e17884cdc9b4ff199a669200fa74ff9d6f895
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-02-09 12:15:55 -0500

    completed pop up

---
+++ front_end/client/src/components/ValidPop.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import accept from '../assets/accept.svg'
+import denied from '../assets/denied.svg'
+function ValidPop(props) {
+    let result = props.result
+    let image = result
+  return (
+      <div className={`r-valid-pop r-v-p-${result}`}>
+          <img src={result=="accept"?accept:denied} alt="" />
+      </div>
+  )
+}
+
+export default ValidPop;
--- front_end/client/src/pages/User.jsx
+++ front_end/client/src/pages/User.jsx
@@ -5,20 +5,21 @@ import Dots from '../assets/dots.svg'
 import Register_two from '../components/Register_two';
 import Register_three from '../components/Register_three';
 import ProgressBar from 'react-bootstrap/ProgressBar'
+import ValidPop from '../components/ValidPop';
 
 
 function User(props) {
   let progress = 20
-
+  let result = "denied"
   return (
       <div className='register1'>
           <Nav title={"Registering"} option={Dots} />
-          <ProgressBar animated striped now={progress} label={`${progress}%`} />
-            {/* <Register_one /> */}
-            {/* <Register_two /> */}
-            <Register_three />
-
-            <button className='r-form-next r-btn' type="submit">Next &#8680;</button>
+          {/* <ProgressBar animated striped now={progress} label={`${progress}%`} /> */}
+          {/* <Register_one /> */}
+          {/* <Register_two /> */}
+          {/* <Register_three /> */}
+          {/* <button className='r-form-next r-btn' type="submit">Next &#8680;</button> */}
+          <ValidPop result={result} />
       </div>
   )
 }
