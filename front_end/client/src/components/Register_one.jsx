743815db1c575dbde4b6d875072755a537687b19
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-02-14 11:07:56 -0500

    deployed

--- front_end/client/src/components/Register_one.jsx
+++ front_end/client/src/components/Register_one.jsx
@@ -11,37 +11,33 @@ function Register_one() {
     const [password, setPassword] = useState("")
     const [passwordVer, setPasswordVer] = useState("")
     const [avatar, setAvatar] = useState("")
-    // const handleFileSelected = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    //     const files = Array.from(e.target.files)
-    //     console.log("files:", files)
-    //   }
-    // useEffect(()=>{
-    //     axios.post('http://localhost:3001/').then(res =>{
-    //         // SetList(res.data)
-    //         // SetNbr(res.data.length)
-    //         console.log(res)
-                    
-    //       })
-    //     },[])
+    const [user, setUser] = useState("")
+
+        axios.post("http://localhost:3001/")
+    useEffect(()=>{
+        axios.get('http://localhost:3001/').then(res =>{
+            setUser(res.data[0].firstname)
+          })
+        },[])
   return (
-      <form method='POST' action="/" className='register register1'>
+      <form method='POST' action={"/"} className='register register1'>
           <div className="r-header">
-          <h1>Hi, Welcome on Slick</h1>
+          <h1>Hi, Welcome on Slick {user}</h1>
           <h3>Let's Set up your slicky Profile</h3>
           </div>
             <div className="r-field">
                 <label htmlFor="">Fisrt Name</label>
-                <input className='' type="text" name='firstname' value={firstName} onChange={()=>setFirstName()} />
+                <input className='' type="text" name='firstname'  />
                 <span className="requiered hide">This is requiered</span>
             </div>
             <div className="r-field">
                 <label htmlFor="" name="lastname">Last Name</label>
-                <input className='' type="text" value={lastName} onChange={()=>setLastName()} />
+                <input className='' type="text" />
                 <span className="requiered hide">This is requiered</span>
             </div>
             <div className="r-field">
                 <label htmlFor="">Email</label>
-                <input className='' type="text" name="email" value={email} onChange={()=>setEmail()} />
+                <input className='' type="text" name="email"  />
                 <span className="requiered hide">This is requiered</span>
             </div>
             <div className="r-field r-field-special">
@@ -60,15 +56,15 @@ function Register_one() {
             </div>
             <div className="r-field">
                 <label htmlFor="">Create Password</label>
-                <input className='' type="text" name='password' value={password} onChange={()=>setPassword()} />
+                <input className='' type="text" name='password' />
                 <span className="requiered hide">This is requiered</span>
             </div>
             <div className="r-field">
                 <label htmlFor="">Verify Password</label>
-                <input className='' type="text" value={passwordVer} onChange={()=>setPasswordVer()} />
+                <input className='' type="text" />
                 <span className="requiered hide">This is requiered</span>
             </div>
-            {/* <button type="submit">click</button> */}
+            <button type="submit">click</button>
             <div className="r-field">
                 <label htmlFor="file" className='r-upload' >Choose Profile image</label>
                 <input className='r-r-f-import' type="file" name="file"/>
